7739a599187486363dca02373b824390f546b972
Daviani <d.fillatre.pro@gmail.com>
2021-09-24 17:21:57 +0200

    configure tailwnindcss for this project

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        yellow: {
+          500: '#c6a279',
+          600: '#786043'
+        }
+      },
+      fontFamily: {
+        Bastille: ['Bastille Vredeburg Demo']
+      }
+    },
+    minHeight: {
+      '0': '0',
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+      'full': '100%',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
