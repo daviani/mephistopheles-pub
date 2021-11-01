d875a089e144f181ee6c6cf1f0b07c44018e94db
Daviani <contact@daviani.dev>
2021-11-01 22:57:55 +0100

    [CREATE] main loading system

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,27 @@
+import {useState, useEffect} from "react";
+import {useRouter} from 'next/router'
+import Loader from '../components/main_loading'
 import 'tailwindcss/tailwind.css'
-import  '../styles/index.css'
+import '../styles/index.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", (url) => {
+      setLoading(true)
+    });
+    router.events.on("routeChangeComplete", (url) => {
+      setLoading(false)
+    });
+  }, []);
+
+  return (
+    <>
+      {loading ? <Loader/> : <Component {...pageProps} />}
+    </>
+  )
 }
 
 export default MyApp
