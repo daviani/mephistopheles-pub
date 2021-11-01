75ca7548359deb49925f94862a8b20abd646c133
Daviani <contact@daviani.dev>
2021-11-01 22:56:46 +0100

    [CREATE] main_loading component

--- components/main_loading.js
+++ components/main_loading.js
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
-    <div className=' flex justify-center items-center'>
-      <div className='animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-yellow-500' />
+    <div className=' flex justify-center items-center mt-20 sm:mt-96 '>
+      <div className='animate-spin rounded-full border-t-2 border-b-2 border-yellow-500 h-10 sm:w-40 w-10 sm:h-40' />
     </div>
   )
 }
