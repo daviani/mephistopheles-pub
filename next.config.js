c7b5c4d3a165e88ba0b0ad5719d987025095b7c1
Daviani <contact@daviani.dev>
2021-12-08 16:45:58 +0100

    [ADD] domain to datocms in nextconfig file

--- next.config.js
+++ next.config.js
@@ -24,6 +24,7 @@ module.exports = withFonts(
             return config
         },
         images: {
+            domains: ['www.datocms-assets.com'],
             formats: ['image/avif', 'image/webp'],
         },
         i18n: {
