3950156d7e40da03198bcfc3aacd1abc3b905391
Daviani <contact@daviani.dev>
2022-02-12 19:11:51 +0100

    Update opening hours content

--- components/main_weekily-special.js
+++ components/main_weekily-special.js
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router'
 
 const WeekSpecial = ({ weekSpecial }) => {
     const { locale } = useRouter()
-    
     return (
         <div className="rounded overflow-hidden text-center p-5">
             <h3 className="text-center text-yellow-500 text-5xl m-8 bastille">
@@ -26,16 +25,16 @@ const WeekSpecial = ({ weekSpecial }) => {
                 {weekSpecial.entry2}
             </p>
             
-            {weekSpecial.length > 1
-                ?? <>
-                    <p>
-                        {i18n.cards.ou[locale]}
-                    </p>
-                    <p className="text-yellow-500 coquette ">
-                        {weekSpecial.entry3}
-                    </p>
-                </>
+            {weekSpecial.entry3.length < 1
+                ? ''
+                : <p>
+                    {i18n.cards.ou[locale]}
+                </p>
             }
+            
+            <p className="text-yellow-500 coquette ">
+                {weekSpecial.entry3}
+            </p>
             
             <p className="mt-2 font-semibold text-lg">
                 {i18n.cards.dishes_title[locale]}
@@ -49,16 +48,17 @@ const WeekSpecial = ({ weekSpecial }) => {
             <p className="text-yellow-500 coquette ">
                 {weekSpecial.dish2}
             </p>
-            {weekSpecial.length > 1
-                ?? <>
-                    <p>
-                        {i18n.cards.ou[locale]}
-                    </p>
-                    <p className="text-yellow-500 coquette ">
-                        {weekSpecial.dish3}
-                    </p>
-                </>
+            
+            {weekSpecial.entry3.length < 1
+                ? ''
+                : <p>
+                    {i18n.cards.ou[locale]}
+                </p>
             }
+            <p className="text-yellow-500 coquette ">
+                {weekSpecial.dish3}
+            </p>
+            
             <p className="mt-2 font-semibold text-lg">
                 {i18n.cards.desserts_title[locale]}
             </p>
@@ -71,16 +71,17 @@ const WeekSpecial = ({ weekSpecial }) => {
             <p className="text-yellow-500 coquette ">
                 {weekSpecial.dessert2}
             </p>
-            {weekSpecial.length > 1
-                ?? <>
-                    <p>
-                        {i18n.cards.ou[locale]}
-                    </p>
-                    <p className="text-yellow-500 coquette ">
-                        {weekSpecial.dessert3}
-                    </p>
-                </>
+            
+            {weekSpecial.entry3.length < 1
+                ? ''
+                : <p>
+                    {i18n.cards.ou[locale]}
+                </p>
             }
+            <p className="text-yellow-500 coquette ">
+                {weekSpecial.dessert3}
+            </p>
+        
         </div>
     )
 }
